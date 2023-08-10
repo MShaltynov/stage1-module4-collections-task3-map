6bbd5424ab684cbcea1b5d700773fe1a1c1d6cb2
Mikhail <shaltynovm@gmail.com>
2023-08-10 19:53:31 +0530

    changed KeyValueSwapper

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -7,7 +7,8 @@ public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
         Map<String, Integer> result = new HashMap<>();
         for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
-            if (!result.containsKey(e.getKey())) {
+            Integer i=e.getKey();
+            if (!result.containsKey(i.toString())) {
                 result.put(e.getValue(), e.getKey());
             }
         }
