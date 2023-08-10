8089591e0899f09e4f278e1fff3fb8a1312f27f5
Mikhail <shaltynovm@gmail.com>
2023-08-10 20:37:43 +0530

    added condition of checking repetitive key

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -7,9 +7,15 @@ public class KeyValueSwapper {
     public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
         Map<String, Integer> result = new HashMap<>();
         for (Map.Entry<Integer, String> e : sourceMap.entrySet()) {
-            Integer i=e.getKey();
-            if (!result.containsKey(i.toString())) {
-                result.put(e.getValue(), e.getKey());
+            Integer key=e.getKey();
+            String value =e.getValue();
+            if (!result.containsKey(value)) {
+                result.put(value, key);
+            }else{
+                if (result.get(value)>key){
+                    result.put(value, key);
+                }
+
             }
         }
 
